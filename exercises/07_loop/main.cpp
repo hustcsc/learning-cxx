93e9e064a3e05eaa5ee97566003caf24710dcd7a
hustcsc <zzucsc@163.com>
2024-12-19 12:48:57 +0800

    Changes to be committed: 	modified:   exercises/05_constexpr/main.cpp 	modified:   exercises/06_array/main.cpp 	modified:   exercises/07_loop/main.cpp 	modified:   exercises/08_pointer/main.cpp 	modified:   exercises/09_enum&union/main.cpp 	modified:   exercises/10_trivial/main.cpp

--- exercises/07_loop/main.cpp
+++ exercises/07_loop/main.cpp
@@ -3,15 +3,18 @@
 // TODO: 改正函数实现，实现正确的缓存优化斐波那契计算
 // THINk: 这个函数是一个纯函数（pure function）吗？
 // READ: 纯函数 <https://zh.wikipedia.org/wiki/%E7%BA%AF%E5%87%BD%E6%95%B0>
-static unsigned long long fibonacci(int i) {
+ static unsigned long long fibonacci(int i) {
     // TODO: 为缓存设置正确的初始值
-    static unsigned long long cache[96], cached;
+    static unsigned long long cache[96]{0,1};
+    static int cached = 1; // 表示已缓存的最大索引
     // TODO: 设置正确的循环条件
-    for (; false; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+    for (; cached < i; ++cached) {
+        cache[cached + 1] = cache[cached] + cache[cached -1];
     }
     return cache[i];
 }
+
+
 
 // ---- 不要修改以下代码 ----
 int main(int argc, char **argv) {
--- exercises/10_trivial/main.cpp
+++ exercises/10_trivial/main.cpp
@@ -3,14 +3,15 @@
 // READ: Trivial type <https://learn.microsoft.com/zh-cn/cpp/cpp/trivial-standard-layout-and-pod-types?view=msvc-170>
 
 struct FibonacciCache {
-    unsigned long long cache[16];
-    int cached;
+    unsigned long long cache[16]{0, 1};
+    int cached = 2;
 };
 
 // TODO: 实现正确的缓存优化斐波那契计算
 static unsigned long long fibonacci(FibonacciCache &cache, int i) {
-    for (; false; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+    
+    for (int cached = cache.cached; cached < i + 1 ; ++cached) {
+        cache.cache[cached] = cache.cache[cached - 1] + cache.cache[cached - 2];
     }
     return cache.cache[i];
 }
@@ -20,6 +21,7 @@ int main(int argc, char **argv) {
     // NOTICE: C/C++ 中，读取未初始化的变量（包括结构体变量）是未定义行为
     // READ: 初始化的各种写法 <https://zh.cppreference.com/w/cpp/language/initialization>
     FibonacciCache fib;
+
     ASSERT(fibonacci(fib, 10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fibonacci(fib, 10) << std::endl;
     return 0;
